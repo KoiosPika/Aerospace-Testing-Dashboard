db75e5063b4ab86966b9b2b928b15cdf1d19abf9
KoiosPika <109259191+KoiosPika@users.noreply.github.com>
2025-03-06 23:24:57 -0800

    fixed firebase_admin json format

--- app/auth.py
+++ app/auth.py
@@ -3,12 +3,22 @@ from firebase_admin import auth, credentials
 from fastapi import Depends, HTTPException, Security
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from dotenv import load_dotenv
-import os
+import os, json
 
 load_dotenv()
 
-cred = credentials.Certificate(os.getenv("FIREBASE_CREDENTIALS"))
-firebase_admin.initialize_app(cred)
+firebase_credentials_path = os.getenv("FIREBASE_CREDENTIALS")
+
+if firebase_credentials_path and os.path.exists(firebase_credentials_path):
+    with open(firebase_credentials_path, "r") as file:
+        try:
+            json.load(file)
+            cred = credentials.Certificate(firebase_credentials_path)
+            firebase_admin.initialize_app(cred)
+        except json.JSONDecodeError as e:
+            raise ValueError(f"Invalid Firebase credentials JSON: {e}")
+else:
+    raise ValueError("FIREBASE_CREDENTIALS file is missing or invalid")
 
 security = HTTPBearer()
 
